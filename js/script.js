69f964273cfde47f4360dfd2b471ea8dd6fa0707
Rodrigo Mancilla <rodmancilla92@gmail.com>
2022-03-04 15:01:57 -0300

    add top collections

--- js/script.js
+++ js/script.js
@@ -16,7 +16,9 @@ let divColecciones = document.getElementById("divColecciones");
 let botonColecciones = document.getElementById("botonColecciones");
 let mensajeExiste = document.getElementById("mensajeExiste");
 let tablaTopColecciones = document.getElementById("tablaTopColecciones")
-
+let modalBody = document.querySelector(".modalBody");
+let divOrdenar=document.querySelector(".divOrdenar")
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 let colecciones = [];
 
 
@@ -33,6 +35,7 @@ else{
 formColecciones.addEventListener('submit', (e) => {
     e.preventDefault();
 
+    //-----defino las variables para almacenar los valores de los inputs
     let imagenColeccion = document.getElementById('imagenColeccion').value;
     let autorImagen = document.getElementById('autor').value;
     let descripcionImagen = document.getElementById('descripcion').value;
@@ -41,7 +44,7 @@ formColecciones.addEventListener('submit', (e) => {
     let tipoBlockchain = document.getElementById('tipoBlockchain').value;
     let precio = document.getElementById('precio').value;
     let cantidadDisponible = document.getElementById('disponibilidad').value;
-    
+    //-----creo una variable de tipo formdata para recorrer cada input del form
     let datForm = new FormData(e.target);
 
     console.log(datForm.get('autor'));
@@ -53,81 +56,165 @@ formColecciones.addEventListener('submit', (e) => {
     console.log(datForm.get('disponibilidad'));
 
 
+    //-------consulto si la coleccion que se va a cargar ya se encuentra en el array de colecciones. Si no se encuentra la agrego
     if (!colecciones.some(coleccionArray => coleccionArray.autorImagen == autorImagen && coleccionArray.descripcionImagen == descripcionImagen)) {
         const coleccion = new Coleccion(autorImagen, descripcionImagen, categoriaImagen, fechaPublicacion, tipoBlockchain, precio, cantidadDisponible);
 
+        //-------agrego el item al array de colecciones
         colecciones.push(coleccion);
 
+        //----------actualizo el localstorage
         localStorage.setItem('colecciones', JSON.stringify(colecciones));
 
+        //---------reseteo el formulario y pongo el foco en el input de Autor
         formColecciones.reset();
 
         mensajeExiste.innerHTML = "";
 
         document.getElementById('autor').focus();
     }
+    //--------si el autor o la coleccion ya existen, muestro un alert con un mensaje de que ya existe.
     else{
         mensajeExiste.innerHTML += `
         <div class="alert alert-warning" role="alert">
             La colección ya existe. Reintente
         </div>`;
     }
+    mostrarColecciones();
 });
 
-//evento para mostrar las colecciones agregadas en la card de colecciones
 
-botonColecciones.addEventListener('click', () =>{
-    divColecciones.innerHTML = "";
-        let coleccionesStorage = JSON.parse(localStorage.getItem('colecciones'));
-        coleccionesStorage.forEach((coleccion, indice) =>{
-            divColecciones.innerHTML += `
-            <div class="card" id="coleccion${indice}">
-            <div class="card-body">
-                <img src="https://images.pexels.com/photos/848573/pexels-photo-848573.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" class="card-img-top imgCard" alt="${coleccion.descripcionImagen}">
-                <h5 class="card-title">Autor: ${coleccion.autorImagen}</h5>
-                <p class="card-text">Descripcion: ${coleccion.descripcionImagen}</p>
-                <p class="card-text">Categoría: ${coleccion.categoriaImagen}</p>
-                <p class="card-text">Fecha publicacion: ${coleccion.fechaPublicacion}</p>
-                <p class="card-text">Precio: ${coleccion.precio} ${coleccion.tipoBlockchain}</p>
-                <p class="card-text">Disponibilidad: ${coleccion.cantidadDisponible}</p>
-                </br>
-                <button class="btn btn-primary" id="botonAgregarCarrito">Agregar 🛒</button>
-                <button class="btn btn-danger" id="botonEliminarDelCarrito">Eliminar</button>
-            </div>
-            </div>
-            `
-        })
-});
+
 
 //-----------mostrar colecciones
 const mostrarColecciones = () =>{
     divColecciones.innerHTML = "";
-        let coleccionesStorage = JSON.parse(localStorage.getItem('colecciones'));
-        coleccionesStorage.forEach((coleccion, indice) =>{
-            divColecciones.innerHTML += `
-            <div class="card" id="coleccion${indice}">
-            <div class="card-body">
-                <img src="https://images.pexels.com/photos/848573/pexels-photo-848573.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" class="card-img-top imgCard" alt="${coleccion.descripcionImagen}">
-                <h5 class="card-title">Autor: ${coleccion.autorImagen}</h5>
-                <p class="card-text">Descripcion: ${coleccion.descripcionImagen}</p>
-                <p class="card-text">Categoría: ${coleccion.categoriaImagen}</p>
-                <p class="card-text">Fecha publicacion: ${coleccion.fechaPublicacion}</p>
-                <p class="card-text">Precio: ${coleccion.precio} ${coleccion.tipoBlockchain}</p>
-                <p class="card-text">Disponibilidad: ${coleccion.cantidadDisponible}</p>
-                </br>
-                <button class="btn btn-primary" id="botonAgregarCarrito">Agregar 🛒</button>
-                <button class="btn btn-danger" id="botonEliminarDelCarrito">Eliminar</button>
-            </div>
-            </div>
-            `
-        })
+    //------defino una variable para buscar en el localstorage la informacion guardada
+    let coleccionesStorage = JSON.parse(localStorage.getItem('colecciones'));
+    //------recorro cada item del array y a partir de cada uno, creo una card mostrando los datos
+    coleccionesStorage.forEach((coleccion, indice) =>{
+        divColecciones.innerHTML += `
+        <div class="card" id="coleccion${indice}">
+        <div class="card-body">
+            <img src="https://images.pexels.com/photos/848573/pexels-photo-848573.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" class="card-img-top imgCard" alt="${coleccion.descripcionImagen}">
+            <h5 class="card-title autor">Autor: ${coleccion.autorImagen}</h5>
+            <p class="card-text descripcion">Descripcion: ${coleccion.descripcionImagen}</p>
+            <p class="card-text categoria">Categoría: ${coleccion.categoriaImagen}</p>
+            <p class="card-text fecha">Fecha publicacion: ${coleccion.fechaPublicacion}</p>
+            <p class="card-text precio">Precio: ${coleccion.precio} ${coleccion.tipoBlockchain}</p>
+            <p class="card-text disponibilidad">Disponibilidad: ${coleccion.cantidadDisponible}</p>
+            </br>
+            <button class="btn-agregar btn btn-dark" id="botonAgregarCarrito" data-id="${indice}">Agregar 🛒</button>
+        </div>
+        </div>
+        `
+    })
 }
 
-//--------obtener colecciones en la tabla
+//------cuando hago click en el boton agregar, llamo al evento click y consulto cual es el padre 
+//-------*En construccion*
+const btnAgregar = document.querySelectorAll(".btn-agregar");
+btnAgregar.forEach((e) =>
+e.addEventListener("click", (e) => {
+    let cardPadre = e.target.parentElement;
+    console.log(cardPadre)
+    agregarAlCarrito(cardPadre);
+})
+);
+//-------funcion para agregar un producto al carrito 
+//-------* En construccion*
 
+const agregarAlCarrito = (cardPadre) => {
+    let coleccion = {
+      imagenColeccion: cardPadre.querySelector("img").src,
+      descripcionImagen: cardPadre.querySelector(".descripcion").textContent,
+      cantidad: 1,
+      precio: Number(cardPadre.querySelector(".precio").textContent),
+      id: Number(cardPadre.querySelector("button").getAttribute("data-id")),
+    };
+  
+    let coleccionEncontrada = carrito.find(
+      (element) => element.id === coleccion.id
+    );
+  
+    if (coleccionEncontrada) {
+      coleccionEncontrada.cantidad++;
+    } else {
+      carrito.push(coleccion);
+    }
+    console.log(carrito);
+    
+  };
 
+//--------obtener colecciones en la tabla a partir del localstorage
 
-//-----------dark mode
+const llenarTabla = () =>{
+    
+    let coleccionesStorage = JSON.parse(localStorage.getItem('colecciones'));
+    coleccionesStorage.forEach((coleccion) =>{
+
+        //---------seteo una imagen dependiendo el tipo de opcion seleccionada
+        let imagen = ""
+        let valorDolar= ""
+        if(coleccion.tipoBlockchain === "ethereum"){
+            imagen="https://img.icons8.com/cotton/30/000000/ethereum--v1.png";
+            valorDolar=2673,91;
+        }
+        else if(coleccion.tipoBlockchain === "bitcoin"){
+            imagen="https://img.icons8.com/color/30/000000/bitcoin--v1.png";
+            valorDolar=40768,60;
+        }
+        else{
+            imagen="https://img.icons8.com/fluency/30/000000/doge.png";
+            valorDolar=0.13;
+        }
+
+        //--------lleno la tabla con los datos obtenidos y los muesro en el DOM
+        let tbody = document.createElement("tr");
+            tbody.className = "tbody";
+            tablaTopColecciones.appendChild(tbody);
+
+            tbody.innerHTML += `
+            <tr class="trTopColecciones">
+                <td class="tdTopColecciones">${coleccion.descripcionImagen}</td>
+                <td class="tdTopColecciones">${coleccion.autorImagen}</td>
+                <td class="tdTopColecciones">${coleccion.precio}    <img src="${imagen}" class="img-top imagenBlockchain" ></img>(U$S<span class="conversionDolar">${coleccion.precio * valorDolar}</span>)</td>
+            </tr>
+                
+            `
+        
+    })
+
+    //----llamo a la funcion mostrarColecciones para mostrar las cards actualizadas
+    mostrarColecciones();
+}
+
+//-------funcion para ordenar lista de colecciones por precio asc
+//-----en construccion
+
+const ordenarPorPrecioAsc = () =>{
+    let coleccionesStorage = JSON.parse(localStorage.getItem('colecciones'));
+
+    coleccionesStorage.sort((coleccion1, coleccion2) => coleccion1.precio - coleccion2.precio);
+    console.log(coleccionesStorage)
+}
+
+//--------funcion para determinar el tipo de orden de los datos
+//------en construccion
+
+const escucharBotonesDivOrdenar = () =>{
+    divOrdenar.addEventListener("click", (e) => {
+        if(e.target.classList.contains("botonOrdenaPrecioAsc")){
+            ordenarPorPrecioAsc();
+        }
+        else if(e.target.classList.contains("botonOrdenaPrecioDesc")){
+            ordenarPorPrecioDesc();
+        }
+        
+    })
+}
+
+//----------- implementa dark mode
 
 function darkMode() {
     let element = document.body;
@@ -136,3 +223,6 @@ function darkMode() {
  }
 
  mostrarColecciones();
+ llenarTabla();
+ escucharBotonesDivOrdenar();
+ ordenarPorPrecioAsc();
